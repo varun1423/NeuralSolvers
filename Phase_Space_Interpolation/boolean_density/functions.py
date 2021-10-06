ed51e33fe10b8521dc39c0934e8756a6c4caeef3
varun1423 <varunshitole@gmail.com>
2021-10-06 15:46:15 +0200

    updated for all series and ions, electrons

--- Phase_Space_Interpolation/boolean_density/functions.py
+++ Phase_Space_Interpolation/boolean_density/functions.py
@@ -8,6 +8,7 @@ def data_set(path, iteration_id=2000):
 
     i = series.iterations[iteration_id]
     electrons = i.particles['e']
+    ions = i.particles['i']
     position_x = electrons["position"]["x"].load_chunk()
     position_y = electrons["position"]["y"].load_chunk()
     position_z = electrons["position"]["z"].load_chunk()
@@ -15,14 +16,36 @@ def data_set(path, iteration_id=2000):
     positionOffset_y = electrons["positionOffset"]["y"].load_chunk()
     positionOffset_z = electrons["positionOffset"]["z"].load_chunk()
     weighting = electrons["weighting"][io.Mesh_Record_Component.SCALAR].load_chunk()
+    # ions
+    ions_position_x = ions["position"]["x"].load_chunk()
+    ions_position_y = ions["position"]["y"].load_chunk()
+    ions_position_z = ions["position"]["z"].load_chunk()
+    ions_positionOffset_x = ions["positionOffset"]["x"].load_chunk()
+    ions_positionOffset_y = ions["positionOffset"]["y"].load_chunk()
+    ions_positionOffset_z = ions["positionOffset"]["z"].load_chunk()
+    ions_weighting = ions["weighting"][io.Mesh_Record_Component.SCALAR].load_chunk()
     series.flush()
 
-    particle_position = np.stack((positionOffset_x, positionOffset_y, positionOffset_z, positionOffset_x + position_x,
-                                  positionOffset_y + position_y,
-                                  positionOffset_z + position_z,
-                                  weighting), axis=1)
-    return particle_position
+    electron_position = np.stack(
+        (positionOffset_x, positionOffset_y, positionOffset_z, positionOffset_x + position_x,
+         positionOffset_y + position_y,
+         positionOffset_z + position_z,
+         weighting), axis=1)
+    ions_position = np.stack(
+        (ions_positionOffset_x, ions_positionOffset_y, ions_positionOffset_z, ions_positionOffset_x + ions_position_x,
+         ions_positionOffset_y + ions_position_y,
+         ions_positionOffset_z + ions_position_z,
+         ions_weighting), axis=1)
+    data_dict = {'electron': electron_position, 'ion': ions_position}
+    return data_dict
 
+def iteration():
+    path = "D:/TUD/TU_Dresden/WiSe2021/Thesis/other/Data_s/004_KHI/simOutput/openPMD/simOutput_%06T.bp"
+    series = io.Series(path, io.Access_Type.read_only)
+    iterations = []
+    for iter_id in series.iterations:
+        iterations.append(iter_id)
+    return iterations
 
 def assignment_function(distance):
     cumulative_assignment_function = 0
@@ -37,21 +60,21 @@ def assignment_function(distance):
     return cumulative_assignment_function
 
 
-def small_search_space(particle_position,particles__offset_x,particles__offset_y,particles__offset_z):
-
-    mask_x = (particle_position[:, 0] >= particles__offset_x-1)
+def small_search_space(particle_position, particles__offset_x, particles__offset_y, particles__offset_z):
+    mask_x = (particle_position[:, 0] >= particles__offset_x - 1)
     output = particle_position[mask_x, :]
-    mask_xx = (output[:, 0] <= particles__offset_x+1)
+    mask_xx = (output[:, 0] <= particles__offset_x + 1)
     output = output[mask_xx, :]
 
-    mask_y = (output[:, 1] >= particles__offset_y-1)
+    mask_y = (output[:, 1] >= particles__offset_y - 1)
     output = output[mask_y, :]
-    mask_yy = (output[:, 1] <= particles__offset_y+1)
+    mask_yy = (output[:, 1] <= particles__offset_y + 1)
     output = output[mask_yy, :]
 
-    mask_z = (output[:, 2] >= particles__offset_z-1)
+    mask_z = (output[:, 2] >= particles__offset_z - 1)
     output = output[mask_z, :]
-    mask_zz = (output[:, 2] <= particles__offset_z+1)
+    mask_zz = (output[:, 2] <= particles__offset_z + 1)
     output = output[mask_zz, :]
     particle_position = output
     return particle_position
+
--- Phase_Space_Interpolation/boolean_density/main.py
+++ Phase_Space_Interpolation/boolean_density/main.py
@@ -2,47 +2,47 @@
 import numpy as np
 import functions
 import time
+
 start_time = time.time()
 
-iteration_id = 2000
+#path = "D:/TUD/TU_Dresden/WiSe2021/Thesis/other/Data_s/004_KHI/simOutput/openPMD/simOutput_%06T.bp"
 path = "/warm_archive/ws/s5960712-ml_currentDeposition/runs/004_KHI/simOutput/openPMD/simOutput_%06T.bp"
-particle_position = functions.data_set(path, iteration_id)
 
-# %%
-# initialize empty list
-dicts = {}
-density = []
+iterations_list = functions.iteration()
+iterations_list = iterations_list[0:4]
 
-for particle in range(0, len(particle_position[:, 0:3])): 
-    particles__offset_x = particle_position[particle,0:3][0]
-    particles__offset_y = particle_position[particle,0:3][1]
-    particles__offset_z = particle_position[particle,0:3][2]
-    
-    neighbouring_cells = functions.small_search_space(particle_position, particles__offset_x,particles__offset_y,particles__offset_z)
-    dicts[particle] = neighbouring_cells
+for iter_id in iterations_list:
+    data = functions.data_set(path, iter_id)
 
-    x_distance = particles__offset_x-dicts[particle][:,3]
-    y_distance = particles__offset_y-dicts[particle][:,4]
-    z_distance = particles__offset_z-dicts[particle][:,5]
-    weighting = particle_position[particle,6]
+    for element in ['electron', 'ion']:
+        particle_position = data[element]
+        # break
+        density = []
+        for particle in range(0, 10):  # len(particle_position[:, 0:3])
+            particles__offset_x = particle_position[particle, 0:3][0]
+            particles__offset_y = particle_position[particle, 0:3][1]
+            particles__offset_z = particle_position[particle, 0:3][2]
 
-    x_density = functions.assignment_function(x_distance)
-    y_density = functions.assignment_function(y_distance)
-    z_density = functions.assignment_function(z_distance)
-    x_density_w = x_density * weighting 
-    y_density_w = y_density * weighting
-    z_density_w = z_density * weighting
-    density.append([x_density, y_density, z_density, x_density_w, y_density_w, z_density_w])
-    if particle%5000 == 0:
-        print('')
-        print('at particle:'.format(particle))
-    if particle%25000 == 0:
+            neighbouring_cells = functions.small_search_space(particle_position, particles__offset_x,
+                                                              particles__offset_y, particles__offset_z)
+
+            x_distance = particles__offset_x - neighbouring_cells[:, 3]
+            y_distance = particles__offset_y - neighbouring_cells[:, 4]
+            z_distance = particles__offset_z - neighbouring_cells[:, 5]
+            weighting = particle_position[particle, 6]
+
+            x_density = functions.assignment_function(x_distance)
+            y_density = functions.assignment_function(y_distance)
+            z_density = functions.assignment_function(z_distance)
+            x_density_w = x_density * weighting
+            y_density_w = y_density * weighting
+            z_density_w = z_density * weighting
+            density.append([x_density, y_density, z_density, x_density_w, y_density_w, z_density_w])
+            if particle % 25000 == 0:
+                calculated_density = np.stack(density, axis=0)
+                np.save('Calculated_Density/density_intermediate_' + element + '_' + str(iter_id), calculated_density)
         calculated_density = np.stack(density, axis=0)
-        np.save('density_intermediate',calculated_density)
-        print('')
-        print('Intermediate file saved at particle'.format(particle))
-calculated_density = np.stack((density),axis=0)
-np.save('density_intermediate',calculated_density)
-
-time_taken = (time.time() - start_time)/3600
-print("time taken:{}".format(time_taken))
+        np.save('Calculated_Density/Calculated_density_' + element + '_' + str(iter_id), calculated_density)
+    print('Density calculation completed for:{}'.format(iter_id))
+time_taken = (time.time() - start_time) / 3600
+print("time taken:{} hours".format(time_taken))
